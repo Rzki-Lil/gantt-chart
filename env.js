7fb91af253e876d4c25fb1b3229db9de18a8ff05
Rzki-Lil <Rzki-Lil@users.noreply.github.com>
2024-11-25 22:00:40 +0000

    deploy: 4157861612e9cef2eed78369e0f2e4b146bffdf5

--- env.js
+++ env.js
@@ -1,7 +1,7 @@
-window.FIREBASE_API_KEY='';
-window.FIREBASE_AUTH_DOMAIN='';
-window.FIREBASE_DATABASE_URL='';
-window.FIREBASE_PROJECT_ID='';
-window.FIREBASE_STORAGE_BUCKET='';
-window.FIREBASE_MESSAGING_SENDER_ID='';
-window.FIREBASE_APP_ID='';
+window.FIREBASE_API_KEY='AIzaSyBA6_9N-MpkZ_kI6qC9L7tEC4EAZQRIrVo';
+window.FIREBASE_AUTH_DOMAIN='latihan1-firebase-a0dfa.firebaseapp.com';
+window.FIREBASE_DATABASE_URL='https://latihan1-firebase-a0dfa-default-rtdb.asia-southeast1.firebasedatabase.app';
+window.FIREBASE_PROJECT_ID='latihan1-firebase-a0dfa';
+window.FIREBASE_STORAGE_BUCKET='latihan1-firebase-a0dfa.firebasestorage.app';
+window.FIREBASE_MESSAGING_SENDER_ID='891995696037';
+window.FIREBASE_APP_ID='1:891995696037:web:e88d1fdf1bad33f6be964c ';
